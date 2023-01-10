0e1c541860fa8f1f81d479eba6f59b372016a892
Syafiq <isyafiq@gmail.com>
2023-01-10 15:38:06 +0800

    changed helloworld message

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Made by Syafiq"/>
   </div>
 </template>
 
