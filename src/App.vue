c828819428f49ef94528effc8ea878b8135040c3
Syafiq <isyafiq@gmail.com>
2023-01-11 11:33:31 +0800

    added scss and footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to='/'>Home</router-link> | 
-    <router-link to='/about'>About</router-link>
+    <router-link to='/about'>About</router-link> | 
+    <router-link to='/footerTest'>FooterTest</router-link>
   </nav>
   <router-view  />
 </template>
@@ -12,12 +13,11 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
@@ -27,14 +27,14 @@ body {
 
 nav {
   padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  text-align: center;
+  > a {
+    font-weight: bold;
+    color: #2c3e50;
+    
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
