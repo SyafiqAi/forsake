9632b5651f96cd94082bc0a56645e055dab3fdf6
Syafiq <isyafiq@gmail.com>
2023-01-14 00:37:56 +0800

    fix navbar

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="top-navbar">
-    <nav>
-      <router-link @click="close=!close" to='/'>Home</router-link>
-      <div>|</div>
-      <router-link @click="close=!close" to='/about'>About</router-link>
-    </nav>
-    <SideMenu :close="close" @toggle="$emit('toggle')"/>
+    <div class="nav-container">
+      <nav>
+        <router-link @click="close=!close" to='/'>Home</router-link>
+        <div>|</div>
+        <router-link @click="close=!close" to='/about'>About</router-link>
+      </nav>
+    </div>
+    <div class="sidemenu-container">
+      <SideMenu :close="close" @toggle="$emit('toggle')"/>
+    </div>
   </div>
 </template>
 
@@ -25,33 +29,46 @@ export default {
 
 .top-navbar {
   height: 50px;
-  width: 100vw;
+  width: 100%;
   position: relative;
   background-color: v-bind('$darkDarkBlue');
-
-  > nav {
-    color: v-bind('$grey');
-    width: 100vw;
+  display: flex;
+  justify-content: center;
+  > .nav-container {
+    width: 100%;
     height: 100%;
-    
+    position: absolute;
     display: flex;
     justify-content: center;
     align-items: center;
-
-    position: absolute;
-    
-    > a {
-      margin: auto 10px;
-      font-weight: bold;
+    > nav {
       color: v-bind('$grey');
+      // width: 100vw;
+      // height: 100%;
       
-      &.router-link-exact-active {
-        color: v-bind('$boldGreen');
-      }
-      &:hover {
-        color: v-bind('$lightGreen');
+      display: flex;
+      justify-content: center;
+      align-items: center;
+  
+      
+      > a {
+        margin: auto 10px;
+        font-weight: bold;
+        color: v-bind('$grey');
+        
+        &.router-link-exact-active {
+          color: v-bind('$boldGreen');
+        }
+        &:hover {
+          color: v-bind('$lightGreen');
+        }
       }
     }
+  }
+  > .sidemenu-container {
+    position: absolute;
+    width: 100%;
+    height: 100%;
   }
 }
 </style>
