d36d1a848b28477a8e34954cf5f8832c42d66443
Syafiq <isyafiq@gmail.com>
2023-01-16 11:16:12 +0800

    scr

--- src/App.vue
+++ src/App.vue
@@ -72,25 +72,25 @@ body {
   height: 100vh;
 }
 
-::-webkit-scrollbar {
-  width: 10px;
-  }
+// ::-webkit-scrollbar {
+//   width: 10px;
+//   }
 
-  /* Track */
-::-webkit-scrollbar-track {
-  background: #242424; 
-  }
+//   /* Track */
+// ::-webkit-scrollbar-track {
+//   background: #242424; 
+//   }
   
-  /* Handle */
-::-webkit-scrollbar-thumb {
-    background: v-bind('$darkDarkBlue'); 
-    background: #090D11; 
-  }
+//   /* Handle */
+// ::-webkit-scrollbar-thumb {
+//     background: v-bind('$darkDarkBlue'); 
+//     background: #090D11; 
+//   }
 
-  /* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  background: rgb(71, 71, 71); 
-}
+//   /* Handle on hover */
+// ::-webkit-scrollbar-thumb:hover {
+//   background: rgb(71, 71, 71); 
+// }
 
 #MathJax_Message {
   display: none !important;
