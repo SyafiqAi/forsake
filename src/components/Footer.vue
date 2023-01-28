183138d1b928f9ce59355359759efcd0fec5dc6e
Syafiq <isyafiq@gmail.com>
2023-01-29 00:17:08 +0800

    update vite

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@
       <br />
       Thank you Mason.
       <br />
-      V 1.2.0
+      V 1.3.0
     </div>
   </div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-var path = require("path");
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
