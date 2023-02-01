3eca0fc3462a08a25e5fab4f317060fad30f00af
Syafiq <isyafiq@gmail.com>
2023-02-01 21:18:46 +0800

    Change footer background color

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -18,8 +18,9 @@ export default {
 
 <style scoped>
     #footer {
-        background-color: v-bind('$boldGreen');
-        color: white;
+        background-color: v-bind('$darkDarkBlue');
+        /* background-color: black; */
+        color: rgb(92, 92, 92);
         width: 100%;
         min-height: 75px;
         margin-top: auto;
